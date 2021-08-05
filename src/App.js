270c2353d702e6991958ab981619f99406919076
edaisaku <edaisaku@yahoo.com>
2021-08-05 14:14:59 +0200

    Added Redux to manage state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import "./App.css";
 import React, { Component } from "react";
 import SearchBox from "./Components/SearchBox/SearchBox";
 import Cards from "./Components/Cards/Cards";
 import Scroll from "./Components/Scroll/Scroll";
 import ErrorBoundry from "./Components/ErrorBoundry/ErrorBoundry";
-import "./App.css";
+
+import { connect } from "react-redux";
+import { setSearchField } from "./actions";
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (ev) => dispatch(setSearchField(ev.target.value)),
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchBox: "",
     };
   }
 
@@ -22,16 +36,11 @@ class App extends Component {
       .then((users) => this.setState({ robots: users }));
   }
 
-  onSearchBoxChange = (ev) => {
-    this.setState({
-      searchBox: ev.target.value,
-    });
-  };
-
   render() {
-    const { robots, searchBox } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((el) => {
-      return el.name.toLowerCase().includes(searchBox.toLowerCase());
+      return el.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     return robots.length === 0 ? (
@@ -39,7 +48,7 @@ class App extends Component {
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchBoxChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
             <Cards robots={filteredRobots} />
@@ -50,4 +59,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import { searchRobots } from "./reducers";
+
+const store = createStore(searchRobots);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
