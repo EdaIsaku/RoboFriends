219827590a608b18352ea6eba0c0344a4e2ed6e7
edaisaku <edaisaku@yahoo.com>
2021-08-05 14:36:09 +0200

    Added a middleware (react-logger) to log changes of state

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+import { createLogger } from "redux-logger";
 
 import { searchRobots } from "./reducers";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
